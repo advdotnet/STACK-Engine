d671d47ae95c2196f40d22bc1e06cdb87fe3e24b
jonas <julian.hobler@posteo.de>
2018-07-27 11:14:09 +0200

    Audiomanager: stop current song if restoring a game state.

--- src/STACK/Components/Audio/AudioManager.cs
+++ src/STACK/Components/Audio/AudioManager.cs
@@ -6,359 +6,369 @@ using System.Collections.Generic;
 
 namespace STACK.Components
 {
-    /// <summary>
-    /// AudioManager class which handles playback of songs and sound effects.
-    /// </summary>
-    [Serializable]
-    public class AudioManager : Component, IContent, IUpdate, IInitialize
-    {
-        public bool Enabled { get; set; }
-        public float UpdateOrder { get; set; }
+	/// <summary>
+	/// AudioManager class which handles playback of songs and sound effects.
+	/// </summary>
+	[Serializable]
+	public class AudioManager : Component, IContent, IUpdate, IInitialize
+	{
+		public bool Enabled { get; set; }
+		public float UpdateOrder { get; set; }
 
-        [NonSerialized]
-        ContentLoader Content;
+		[NonSerialized]
+		ContentLoader Content;
 
-        [NonSerialized]
-        Dictionary<string, SoundEffect> SoundEffects = new Dictionary<string, SoundEffect>();
+		[NonSerialized]
+		Dictionary<string, SoundEffect> SoundEffects = new Dictionary<string, SoundEffect>();
 
-        [NonSerialized]
-        Dictionary<string, Song> Songs = new Dictionary<string, Song>();
+		[NonSerialized]
+		Dictionary<string, Song> Songs = new Dictionary<string, Song>();
 
-        [NonSerialized]
-        List<SoundEffectInstance> PlayingInstances = new List<SoundEffectInstance>();
+		[NonSerialized]
+		List<SoundEffectInstance> PlayingInstances = new List<SoundEffectInstance>();
 
-        [NonSerialized]
-        Song CurrentSong;
+		[NonSerialized]
+		Song CurrentSong;
 
-        [NonSerialized]
-        bool _IsEnginePaused;
+		[NonSerialized]
+		bool _IsEnginePaused;
 
-        string CurrentSongName;
+		string CurrentSongName;
 
-        float _SoundEffectVolume = 1;
-        float _MusicVolume = 1;
+		float _SoundEffectVolume = 1;
+		float _MusicVolume = 1;
 
-        float _MaxSoundEffectVolume = 1;
-        float _MaxMusicVolume = 1;
+		float _MaxSoundEffectVolume = 1;
+		float _MaxMusicVolume = 1;
 
-        MediaState LastMediaState;
-        bool IsRepeating = false;
+		MediaState LastMediaState;
+		bool IsRepeating = false;
 
-        [NonSerialized]
-        ISkipContent SkipContent = null;
+		[NonSerialized]
+		ISkipContent SkipContent = null;
 
-        public void Initialize(bool restore)
-        {
-            SkipContent = ((World)Parent).Get<SkipContent>();
+		public void Initialize(bool restore)
+		{
+			SkipContent = ((World)Parent).Get<SkipContent>();
 
-            if (null != CurrentSongName)
-            {
-                LoadSong(CurrentSongName);
-                if (MediaState.Playing == LastMediaState)
-                {
-                    PlaySong(CurrentSongName);
-                    RepeatSong = IsRepeating;
-                }
-            }
-        }
+			if (null != CurrentSongName)
+			{
+				LoadSong(CurrentSongName);
+				if (MediaState.Playing == LastMediaState)
+				{
+					PlaySong(CurrentSongName);
+					RepeatSong = IsRepeating;
 
-        public AudioManager()
-        {
-            UpdateOrder = 500;
-            Enabled = true;
-        }
+					return;
+				}
+			}
 
-        public bool IsEnginePaused
-        {
-            get
-            {
-                return _IsEnginePaused;
-            }
-            set
-            {
-                _IsEnginePaused = value;
-            }
-        }
+			if (restore)
+			{
+				StopSong();
+			}
+		}
 
-        public float MusicVolume
-        {
-            get
-            {
-                return MediaPlayer.Volume;
-            }
-            set
-            {
-                _MusicVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
-                MediaPlayer.Volume = value * MaxMusicVolume;
-            }
-        }
+		public AudioManager()
+		{
+			UpdateOrder = 500;
+			Enabled = true;
+		}
 
-        public float SoundEffectVolume
-        {
-            get
-            {
-                return _SoundEffectVolume;
-            }
-            set
-            {
-                _SoundEffectVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
-            }
-        }
+		public bool IsEnginePaused
+		{
+			get
+			{
+				return _IsEnginePaused;
+			}
+			set
+			{
+				_IsEnginePaused = value;
+			}
+		}
 
-        public float MaxMusicVolume
-        {
-            get
-            {
-                return _MaxMusicVolume;
-            }
-            set
-            {
-                _MaxMusicVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
-                MediaPlayer.Volume = MusicVolume * value;
-            }
-        }
+		public float MusicVolume
+		{
+			get
+			{
+				return MediaPlayer.Volume;
+			}
+			set
+			{
+				_MusicVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
+				MediaPlayer.Volume = value * MaxMusicVolume;
+			}
+		}
 
-        public float MaxSoundEffectVolume
-        {
-            get
-            {
-                return _MaxSoundEffectVolume;
-            }
-            set
-            {
-                _MaxSoundEffectVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
-            }
-        }
+		public float SoundEffectVolume
+		{
+			get
+			{
+				return _SoundEffectVolume;
+			}
+			set
+			{
+				_SoundEffectVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
+			}
+		}
 
-        public float EffectiveSoundEffectVolume
-        {
-            get
-            {
-                return _MaxSoundEffectVolume * _SoundEffectVolume;
-            }
-        }
+		public float MaxMusicVolume
+		{
+			get
+			{
+				return _MaxMusicVolume;
+			}
+			set
+			{
+				_MaxMusicVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
+				MediaPlayer.Volume = MusicVolume * value;
+			}
+		}
 
-        public void ApplyGameSettingsVolume(GameSettings gameSettings)
-        {
-            if (null != gameSettings)
-            {
-                MaxSoundEffectVolume = gameSettings.SoundEffectVolume;
-                MaxMusicVolume = gameSettings.MusicVolume;
-            }
-        }
+		public float MaxSoundEffectVolume
+		{
+			get
+			{
+				return _MaxSoundEffectVolume;
+			}
+			set
+			{
+				_MaxSoundEffectVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
+			}
+		}
 
-        private void OnMediaStateChanged(object sender, EventArgs e)
-        {
-            // if the media player state was paused due to an engine pause, don't 
-            // store the last state.
+		public float EffectiveSoundEffectVolume
+		{
+			get
+			{
+				return _MaxSoundEffectVolume * _SoundEffectVolume;
+			}
+		}
 
-            if (MediaPlayer.State == MediaState.Paused && IsEnginePaused)
-            {
-                return;
-            }
+		public void ApplyGameSettingsVolume(GameSettings gameSettings)
+		{
+			if (null != gameSettings)
+			{
+				MaxSoundEffectVolume = gameSettings.SoundEffectVolume;
+				MaxMusicVolume = gameSettings.MusicVolume;
+			}
+		}
 
-            LastMediaState = MediaPlayer.State;
-            if (MediaState.Stopped == LastMediaState)
-            {
-                CurrentSongName = null;
-                CurrentSong = null;
-            }
-        }
+		private void OnMediaStateChanged(object sender, EventArgs e)
+		{
+			// if the media player state was paused due to an engine pause, don't 
+			// store the last state.
 
-        public void LoadContent(ContentLoader content)
-        {
-            var Service = ((World)Parent).Get<ServiceProvider>();
+			if (MediaPlayer.State == MediaState.Paused && IsEnginePaused)
+			{
+				return;
+			}
 
-            if (Service != null && Service.Provider != null)
-            {
-                Content = new ContentLoader(Service.Provider, EngineVariables.ContentPath);
-            }
+			LastMediaState = MediaPlayer.State;
+			if (MediaState.Stopped == LastMediaState)
+			{
+				CurrentSongName = null;
+				CurrentSong = null;
+			}
+		}
 
-            SoundEffects = new Dictionary<string, SoundEffect>();
-            Songs = new Dictionary<string, Song>();
-            PlayingInstances = new List<SoundEffectInstance>();
+		public void LoadContent(ContentLoader content)
+		{
+			var Service = ((World)Parent).Get<ServiceProvider>();
 
-            MediaPlayer.MediaStateChanged += this.OnMediaStateChanged;
-        }
+			if (Service != null && Service.Provider != null)
+			{
+				Content = new ContentLoader(Service.Provider, EngineVariables.ContentPath);
+			}
 
-        /// <summary>
-        /// Starts playing a song if not in fast forward mode.
-        /// </summary>
-        /// <param name="song"></param>
-        public void PlaySong(string song)
-        {
-            if (null != SkipContent.SkipCutscene && SkipContent.SkipCutscene.Enabled)
-            {
-                return;
-            }
+			SoundEffects = new Dictionary<string, SoundEffect>();
+			Songs = new Dictionary<string, Song>();
+			PlayingInstances = new List<SoundEffectInstance>();
 
-            LoadSong(song);
+			MediaPlayer.MediaStateChanged += this.OnMediaStateChanged;
+		}
 
-            MediaPlayer.Play(CurrentSong);
-        }
+		/// <summary>
+		/// Starts playing a song if not in fast forward mode.
+		/// </summary>
+		/// <param name="song"></param>
+		public void PlaySong(string song)
+		{
+			if (null != SkipContent.SkipCutscene && SkipContent.SkipCutscene.Enabled)
+			{
+				return;
+			}
 
-        private void LoadSong(string song)
-        {
-            if (!Songs.ContainsKey(song))
-            {
-                Songs[song] = Content.Load<Song>(song);
-            }
+			LoadSong(song);
 
-            CurrentSong = Songs[song];
-            CurrentSongName = song;
-        }
+			MediaPlayer.Play(CurrentSong);
+		}
 
-        public bool RepeatSong
-        {
-            get
-            {
-                return MediaPlayer.IsRepeating;
-            }
-            set
-            {
-                MediaPlayer.IsRepeating = value;
-                IsRepeating = value;
-            }
-        }
+		private void LoadSong(string song)
+		{
+			if (!Songs.ContainsKey(song))
+			{
+				Songs[song] = Content.Load<Song>(song);
+			}
 
-        public void PauseSong()
-        {
-            MediaPlayer.Pause();
-        }
+			CurrentSong = Songs[song];
+			CurrentSongName = song;
+		}
 
-        public void ResumeSong()
-        {
-            if (CurrentSongName != null && MediaPlayer.State == MediaState.Stopped)
-            {
-                PlaySong(CurrentSongName);
-            }
-            else
-            {
-                MediaPlayer.Resume();
-            }
-        }
+		public bool RepeatSong
+		{
+			get
+			{
+				return MediaPlayer.IsRepeating;
+			}
+			set
+			{
+				MediaPlayer.IsRepeating = value;
+				IsRepeating = value;
+			}
+		}
 
-        public void StopSong()
-        {
-            MediaPlayer.Stop();
+		public void PauseSong()
+		{
+			MediaPlayer.Pause();
+		}
 
-            CurrentSong = null;
-            CurrentSongName = null;
-        }
+		public void ResumeSong()
+		{
+			if (CurrentSongName != null && MediaPlayer.State == MediaState.Stopped)
+			{
+				PlaySong(CurrentSongName);
+			}
+			else
+			{
+				MediaPlayer.Resume();
+			}
+		}
 
-        public void LoadSoundEffect(string soundEffect)
-        {
-            if (!SoundEffects.ContainsKey(soundEffect))
-            {
-                SoundEffects[soundEffect] = Content.Load<SoundEffect>(soundEffect);
-            }
-        }
+		public void StopSong()
+		{
+			MediaPlayer.Stop();
 
-        /// <summary>
-        /// Starts playing a sound effect, if not in fast forward mode.
-        /// </summary>
-        /// <param name="soundEffect"></param>
-        /// <returns>SoundEffectInstance, or null if in fast forward mode</returns>
-        public SoundEffectInstance PlaySoundEffect(string soundEffect, bool looped = false, AudioEmitter emitter = null, AudioListener listener = null)
-        {
-            if (null != SkipContent.SkipCutscene && SkipContent.SkipCutscene.Enabled)
-            {
-                return null;
-            }
+			CurrentSong = null;
+			CurrentSongName = null;
+		}
 
-            LoadSoundEffect(soundEffect);
-            var Instance = SoundEffects[soundEffect].CreateInstance();
+		public void LoadSoundEffect(string soundEffect)
+		{
+			if (!SoundEffects.ContainsKey(soundEffect))
+			{
+				SoundEffects[soundEffect] = Content.Load<SoundEffect>(soundEffect);
+			}
+		}
 
-            PlayingInstances.Add(Instance);
+		/// <summary>
+		/// Starts playing a sound effect, if not in fast forward mode.
+		/// </summary>
+		/// <param name="soundEffect"></param>
+		/// <returns>SoundEffectInstance, or null if in fast forward mode</returns>
+		public SoundEffectInstance PlaySoundEffect(string soundEffect, bool looped = false, AudioEmitter emitter = null, AudioListener listener = null)
+		{
+			if (null != SkipContent.SkipCutscene && SkipContent.SkipCutscene.Enabled)
+			{
+				return null;
+			}
 
-            Instance.Volume = EffectiveSoundEffectVolume;
-            Instance.IsLooped = looped;
-            if (null != emitter && null != listener)
-            {
-                Instance.Apply3D(listener.Listener, emitter.Emitter);
-            }
+			LoadSoundEffect(soundEffect);
+			var Instance = SoundEffects[soundEffect].CreateInstance();
+
+			PlayingInstances.Add(Instance);
+
+			Instance.Volume = EffectiveSoundEffectVolume;
+			Instance.IsLooped = looped;
+			if (null != emitter && null != listener)
+			{
+				Instance.Apply3D(listener.Listener, emitter.Emitter);
+			}
 
 
-            if (!SoundDisabled)
-            {
-                Instance.Play();
-            }
-            else
-            {
-                Instance.Stop();
-            }
+			if (!SoundDisabled)
+			{
+				Instance.Play();
+			}
+			else
+			{
+				Instance.Stop();
+			}
 
-            return Instance;
-        }
+			return Instance;
+		}
 
-        private bool SoundDisabled
-        {
-            get
-            {
-                return "1" == Environment.GetEnvironmentVariable("FNA_AUDIO_DISABLE_SOUND");
-            }
-        }
+		const string SOUND_DISABLED_VALUE = "1";
+		const string SOUND_DISABLED_KEY = "FNA_AUDIO_DISABLE_SOUND";
 
-        public void UnloadContent()
-        {
-            MediaPlayer.MediaStateChanged -= this.OnMediaStateChanged;
-            StopSong();
+		private bool SoundDisabled
+		{
+			get
+			{
+				return SOUND_DISABLED_VALUE == Environment.GetEnvironmentVariable(SOUND_DISABLED_KEY);
+			}
+		}
 
-            SoundEffects.Clear();
-            Songs.Clear();
+		public void UnloadContent()
+		{
+			MediaPlayer.MediaStateChanged -= this.OnMediaStateChanged;
+			StopSong();
 
-            if (Content != null)
-            {
-                Content.Unload();
-            }
-        }
+			SoundEffects.Clear();
+			Songs.Clear();
 
-        public void Update()
-        {
-            if (null != SkipContent.SkipCutscene && SkipContent.SkipCutscene.Enabled)
-            {
-                StopAll();
-            }
+			if (Content != null)
+			{
+				Content.Unload();
+			}
+		}
 
-            if (PlayingInstances.Count > 0)
-            {
-                for (int i = PlayingInstances.Count - 1; i >= 0; i--)
-                {
-                    if (SoundState.Stopped == PlayingInstances[i].State)
-                    {
-                        PlayingInstances.RemoveAt(i);
-                    }
-                }
-            }
-        }
+		public void Update()
+		{
+			if (null != SkipContent.SkipCutscene && SkipContent.SkipCutscene.Enabled)
+			{
+				StopAll();
+			}
 
-        /// <summary>
-        /// Stops all sounds and the current song.
-        /// </summary>
-        public void StopAll()
-        {
-            if (CurrentSong != null && MediaPlayer.State == MediaState.Playing)
-            {
-                StopSong();
-            }
+			if (PlayingInstances.Count > 0)
+			{
+				for (int i = PlayingInstances.Count - 1; i >= 0; i--)
+				{
+					if (SoundState.Stopped == PlayingInstances[i].State)
+					{
+						PlayingInstances.RemoveAt(i);
+					}
+				}
+			}
+		}
 
-            foreach (var Instance in PlayingInstances)
-            {
-                if (Instance.IsPlaying())
-                {
-                    Instance.Stop();
-                }
-            }
-        }
+		/// <summary>
+		/// Stops all sounds and the current song.
+		/// </summary>
+		public void StopAll()
+		{
+			if (CurrentSong != null && MediaPlayer.State == MediaState.Playing)
+			{
+				StopSong();
+			}
 
-        public static AudioManager Create(World addTo)
-        {
-            return addTo.Add<AudioManager>();
-        }
+			foreach (var Instance in PlayingInstances)
+			{
+				if (Instance.IsPlaying())
+				{
+					Instance.Stop();
+				}
+			}
+		}
 
-        public AudioManager SetSongVolume(float val) { MusicVolume = val; return this; }
-        public AudioManager SetSoundEffectVolume(float val) { SoundEffectVolume = val; return this; }
-        public AudioManager SetMaxSongVolume(float val) { MaxMusicVolume = val; return this; }
-        public AudioManager SetMaxSoundEffectVolume(float val) { MaxSoundEffectVolume = val; return this; }
-    }
+		public static AudioManager Create(World addTo)
+		{
+			return addTo.Add<AudioManager>();
+		}
+
+		public AudioManager SetSongVolume(float val) { MusicVolume = val; return this; }
+		public AudioManager SetSoundEffectVolume(float val) { SoundEffectVolume = val; return this; }
+		public AudioManager SetMaxSongVolume(float val) { MaxMusicVolume = val; return this; }
+		public AudioManager SetMaxSoundEffectVolume(float val) { MaxSoundEffectVolume = val; return this; }
+	}
 }
